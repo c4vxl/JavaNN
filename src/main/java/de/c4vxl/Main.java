6a0c42b28fe75a3b38ef44f852ff2ed64f0c8ba1
c4vxl <manage@c4vxl.de>
2024-09-26 19:24:24 +0200

    implementing element wise operations

--- src/main/java/de/c4vxl/Main.java
+++ src/main/java/de/c4vxl/Main.java
@@ -4,6 +4,8 @@ import de.c4vxl.engine.data.Tensor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Tensor.of(0, 10, 10));
+        Tensor<Double> x = new Tensor<Double>(10, 10).fill(1.0);
+
+        System.out.println(Tensor.of(10, 5, 5));
     }
 }
--- src/main/java/de/c4vxl/engine/data/Tensor.java
+++ src/main/java/de/c4vxl/engine/data/Tensor.java
@@ -3,11 +3,12 @@ package de.c4vxl.engine.data;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.function.BiFunction;
 
 @SuppressWarnings("unchecked")
 public class Tensor<T> {
     // default data type
-    public static Class<?> defaultDType = Float.class;
+    public static Class<?> defaultDType = Double.class;
 
 
     public T[] data;
@@ -51,7 +52,9 @@ public class Tensor<T> {
     /**
      * Construct a tensor filled with a set object
      */
-    public static Tensor<?> of(Object obj, int... shape) { return new Tensor<>(obj.getClass(), shape).fill(obj); }
+    public static <T> Tensor<T> of(T obj, int... shape) {
+        return new Tensor<T>((Class<T>) obj.getClass(), shape).fill(obj);
+    }
 
     /**
      * Helper function for converting the shape of a tensor to the amount of data which can be stored in it
@@ -61,9 +64,149 @@ public class Tensor<T> {
     /**
      * Fill this Tensor with values
      */
-    public Tensor<?> fill(Object obj) {
+    public Tensor<T> fill(T obj) {
         Arrays.fill(data, obj);
         return this;
+    }
+
+    /**
+     * Run an operation element wise
+     */
+    public Tensor<T> elementWiseIndexed(BiFunction<T, Integer, Object> task) {
+        for (int i = 0; i < this.data.length; i++) {
+            this.data[i] = (T) task.apply(this.data[i], i);
+        }
+
+        return this;
+    }
+
+    /**
+     * Element wise addition
+     */
+    public Tensor<T> add(Tensor<T> other) {
+        T[] otherData = other.data;
+        return elementWiseIndexed((a, index) -> {
+            T b = otherData[index];
+
+            return a instanceof Double ? ((Number) a).doubleValue() + ((Number) b).doubleValue()
+                            : a instanceof Integer ? ((Number) a).intValue() + ((Number) b).intValue()
+                            : a instanceof Float ? ((Number) a).floatValue() + ((Number) b).floatValue()
+                            : a instanceof Long ? ((Number) a).longValue() + ((Number) b).longValue()
+                    : b;
+        });
+    }
+
+    /**
+     * Element wise subtraction
+     */
+    public Tensor<T> sub(Tensor<T> other) {
+        T[] otherData = other.data;
+        return elementWiseIndexed((a, index) -> {
+            T b = otherData[index];
+
+            return a instanceof Double ? ((Number) a).doubleValue() - ((Number) b).doubleValue()
+                    : a instanceof Integer ? ((Number) a).intValue() - ((Number) b).intValue()
+                    : a instanceof Float ? ((Number) a).floatValue() - ((Number) b).floatValue()
+                    : a instanceof Long ? ((Number) a).longValue() - ((Number) b).longValue()
+                    : b;
+        });
+    }
+
+    /**
+     * Element wise division
+     */
+    public Tensor<T> div(Tensor<T> other) {
+        T[] otherData = other.data;
+        return elementWiseIndexed((a, index) -> {
+            T b = otherData[index];
+
+            return a instanceof Double ? ((Number) a).doubleValue() / ((Number) b).doubleValue()
+                    : a instanceof Integer ? ((Number) a).intValue() / ((Number) b).intValue()
+                    : a instanceof Float ? ((Number) a).floatValue() / ((Number) b).floatValue()
+                    : a instanceof Long ? ((Number) a).longValue() / ((Number) b).longValue()
+                    : b;
+        });
+    }
+
+    /**
+     * Element wise multiplication
+     */
+    public Tensor<T> mul(Tensor<T> other) {
+        T[] otherData = other.data;
+        return elementWiseIndexed((a, index) -> {
+            T b = otherData[index];
+
+            return a instanceof Double ? ((Number) a).doubleValue() * ((Number) b).doubleValue()
+                    : a instanceof Integer ? ((Number) a).intValue() * ((Number) b).intValue()
+                    : a instanceof Float ? ((Number) a).floatValue() * ((Number) b).floatValue()
+                    : a instanceof Long ? ((Number) a).longValue() * ((Number) b).longValue()
+                    : b;
+        });
+    }
+
+    /**
+     * Element wise square root
+     */
+    public Tensor<T> sqrt() {
+        if (dtype != Double.class)
+            throw new RuntimeException("Operation can only be performed on dtype 'Double'");
+
+        return this.elementWiseIndexed((a, index) -> {
+            return Math.sqrt((Double) a);
+        });
+    }
+
+    /**
+     * Element wise raise to power
+     */
+    public Tensor<T> pow(double pow) {
+        if (dtype != Double.class)
+            throw new RuntimeException("Operation can only be performed on dtype 'Double'");
+
+        return this.elementWiseIndexed((a, index) -> {
+            return Math.pow((double) a, pow);
+        });
+    }
+
+    /**
+     * Element wise logarithm
+     */
+    public Tensor<T> log() {
+        if (dtype != Double.class)
+            throw new RuntimeException("Operation can only be performed on dtype 'Double'");
+
+        return this.elementWiseIndexed((a, index) -> {
+            return Math.log((double) a);
+        });
+    }
+
+    /**
+     * Element wise clip
+     */
+    public Tensor<T> clip(T min, T max) {
+        if (dtype == Boolean.class)
+            throw new RuntimeException("Operation can not be performed on dtype 'Boolean'");
+
+        return elementWiseIndexed((a, index) -> {
+            if (a instanceof Double) {
+                a = (Double) a > (Double) max ? max : a;
+                return (Double) a < (Double) min ? min : a;
+            } else if (a instanceof Float) {
+                a = (Float) a > (Float) max ? max : a;
+                return (Float) a < (Float) min ? min : a;
+            } else if (a instanceof Long) {
+                a = (Long) a > (Long) max ? max : a;
+                return (Long) a < (Long) min ? min : a;
+            } else {
+                a = (Integer) a > (Integer) max ? max : a;
+                return (Integer) a < (Integer) min ? min : a;
+            }
+        });
+    }
+
+    @Override
+    public Tensor<T> clone() {
+        return new Tensor<>(data.clone(), shape.clone());
     }
 
     @Override
