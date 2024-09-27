290d22853b06f2ffa747f1020e50f08bfde16446
c4vxl <manage@c4vxl.de>
2024-09-27 22:51:15 +0200

    added MSE to example

--- src/main/java/de/c4vxl/Main.java
+++ src/main/java/de/c4vxl/Main.java
@@ -1,10 +1,14 @@
 package de.c4vxl;
 
+import de.c4vxl.engine.data.LossFunction;
 import de.c4vxl.engine.data.Tensor;
 import de.c4vxl.engine.nn.MLP;
 
 public class Main {
     public static void main(String[] args) {
+        // This is an example of how Training could look like
+        // when running you can see the loss decreasing as the model learns to produce the wanted shape ($wanted)
+
         MLP model = new MLP(4, 10, 1, 5);
         model.load("model.mdl");
         Tensor<Double> wanted = Tensor.of(0.0, 1, 10);
@@ -14,7 +18,7 @@ public class Main {
 
         for (int i = 0; i < 5; i++) {
             Tensor<Double> out = model.forward(input);
-            Tensor<Double> loss = out.sub(wanted).pow(2.0).sum(0);
+            Tensor<Double> loss = LossFunction.meanSquaredError(out, wanted);
             System.out.println(loss.item());
 
             Tensor<Double> gradient = out.sub(wanted).mul(2.0);
