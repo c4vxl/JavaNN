287a83286901aae654876d6f447522acf25df6b8
c4vxl <manage@c4vxl.de>
2024-09-28 16:53:48 +0200

    added app for manual benchmarking

--- src/main/java/de/c4vxl/DrawingApp.java
+++ src/main/java/de/c4vxl/DrawingApp.java
@@ -103,7 +103,7 @@ public class DrawingApp extends JFrame {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
 
         for (int y = 0; y < height; y++) for (int x = 0; x < width; x++)
-                image.setRGB(x, y, (int) (pixels[y * width + x] * 255));
+            image.getRaster().setSample(x, y, 0, (int) (pixels[y * width + x] * 1));
 
         try {
             File outputFile = new File(path);
